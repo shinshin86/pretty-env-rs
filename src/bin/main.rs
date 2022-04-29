1fffcab1b915328e8bd48f86311d4d5138a36e0a
shinshin86 <beagles1986@gmail.com>
2022-04-29 17:30:56 +0900

    Fix clippy warning

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -22,11 +22,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let mut option = "";
 
-    if args.len() == 2 {
-        option = &args[1];
-    } else if args.len() > 2 {
-        display_usage();
-        std::process::exit(0);
+    match args.len() {
+        2 => option = &args[1],
+        1 => (),
+        _ => {
+            display_usage();
+            std::process::exit(0);
+        }
     }
 
     let valid_options = ["", "raw", "csv", "json"];
